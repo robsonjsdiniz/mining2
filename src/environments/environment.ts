90f583f9fef28fed6466c0d72c8a37e04ba9c7a4
Robson Diniz <robsonjsdiniz@gmail.com>
2019-03-10 22:08:47 -0300

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,7 @@ export const environment = {
   production: false,
   firebaseConfig: {
 
-    apiKey: 'AIzaSyC3TKVhW9DAngLQuvtHgfH-t0WErIvrL3A',
-    authDomain: 'testproject-56c33.firebaseapp.com',
-    databaseURL: 'https://testproject-56c33.firebaseio.com',
-    projectId: 'testproject-56c33',
-    storageBucket: 'testproject-56c33.appspot.com',
-    messagingSenderId: '129335660216'
+   
   }
 };
 
